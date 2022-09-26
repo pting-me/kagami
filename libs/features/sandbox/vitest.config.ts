9497466ee19c4c5cc0e06fdee9e9351cbe88e635
Philip Ting <philipjting@yahoo.com>
2022-09-26 08:07:30 -0700

    converted sandbox to vitest

---
+++ libs/features/sandbox/vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vitest/config';
+import path from 'path';
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    cache: {
+      dir: path.resolve(
+        __dirname,
+        '../../../node_modules/.vitest/figma-plugin-sandbox'
+      ),
+    },
+  },
+  root: path.resolve(__dirname, 'src'),
+});
