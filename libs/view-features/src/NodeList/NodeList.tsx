7bbce49bd411fe965ccb3cdc8558ba84ae512f79
Philip Ting <philipjting@yahoo.com>
2022-09-27 19:48:55 -0700

    added accordion display and styles

--- libs/view-features/src/NodeList/NodeList.tsx
+++ libs/view-features/src/NodeList/NodeList.tsx
@@ -1,3 +1,25 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+} from '@reach/accordion';
+import '@reach/accordion/styles.css';
+
+const MockExpandIcon = () => (
+  <div
+    style={{
+      width: '16px',
+      height: '16px',
+      display: 'flex',
+      alignItems: 'center',
+    }}
+  >
+    <div className="plus-icon">+</div>
+    <div className="minus-icon">-</div>
+  </div>
+);
+
 function NodeList(props: {
   nodes: BaseNode[];
   type: 'COMPONENT_SET' | 'COMPONENT';
@@ -5,26 +27,47 @@ function NodeList(props: {
   const { nodes, type } = props;
 
   return (
-    <div>
+    <Accordion collapsible multiple>
       {nodes.map((node) => {
-        console.log(node, type);
         if (node.type !== type) {
           return null;
         }
-
         return (
-          <>
-            <div>{node.name}</div>
+          <AccordionItem>
             {type === 'COMPONENT_SET' && (
-              <NodeList
-                nodes={node.children as ComponentNode[]}
-                type="COMPONENT"
-              />
+              <>
+                <AccordionButton style={{ width: '100%', height: '32px' }}>
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <MockExpandIcon />
+                    <div>{node.name}</div>
+                  </div>
+                </AccordionButton>
+
+                <AccordionPanel>
+                  <NodeList
+                    nodes={node.children as ComponentNode[]}
+                    type="COMPONENT"
+                  />
+                </AccordionPanel>
+              </>
             )}
-          </>
+            {type === 'COMPONENT' && (
+              <button
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  height: '32px',
+                  width: '100%',
+                }}
+              >
+                <div style={{ width: '16px' }}></div>
+                {node.name}
+              </button>
+            )}
+          </AccordionItem>
         );
       })}
-    </div>
+    </Accordion>
   );
 }
 
