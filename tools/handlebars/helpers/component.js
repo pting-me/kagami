13a86503303b60155e84a829712eeab8882a7e77
Philip Ting <philipjting@yahoo.com>
2022-09-29 18:10:31 -0700

    added automatic property templating

--- tools/handlebars/helpers/component.js
+++ tools/handlebars/helpers/component.js
@@ -10,6 +10,37 @@ componentHelpers.register = function (Handlebars) {
   Handlebars.registerHelper('pascalcase', function (text) {
     return upperfirst(camelcase(text));
   });
+
+  Handlebars.registerHelper('mapProperties', function (variantGroupProperties) {
+    return Object.entries(variantGroupProperties).map(
+      ([rawProperty, { values }]) => {
+        const property = camelcase(rawProperty);
+        const firstValue = values[0];
+
+        if (firstValue === 'true' || firstValue === 'false') {
+          return {
+            property,
+            isBoolean: true,
+          };
+        } else if (!Number.isNaN(Number(firstValue))) {
+          return {
+            property,
+            isNumber: true,
+          };
+        }
+
+        return {
+          property: camelcase(property),
+          isString: true,
+          values,
+        };
+      }
+    );
+  });
+
+  Handlebars.registerHelper('mapValues', function (values) {
+    return Object.entries(values).map((value) => camelcase(value));
+  });
 };
 
 module.exports = componentHelpers;
