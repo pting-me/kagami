4b9594f5b476ed17003bda85ada6359df00105a0
Philip Ting <pting.me+gh@gmail.com>
2023-04-29 10:29:36 -0700

    configured build scripts

--- packages/sandbox/esbuild.config.cjs
+++ packages/sandbox/esbuild.config.cjs
@@ -1,13 +1,16 @@
 const esbuild = require("esbuild");
 const path = require("node:path");
+const yargs = require("yargs");
+
+const isDev = yargs.argv.mode === "dev";
 
 esbuild
   .build({
     entryPoints: ["src/main.ts"],
     outdir: path.resolve(__dirname, "../../dist"),
     bundle: true,
-    sourcemap: true,
-    minify: false,
+    sourcemap: isDev,
+    minify: !isDev,
     format: "iife",
     target: ["chrome58", "safari11"],
     platform: "node",
