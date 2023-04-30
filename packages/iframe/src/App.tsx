83af51c1e414ee690a9ab8c21591f26753c97cda
Philip Ting <pting.me+gh@gmail.com>
2023-04-29 21:46:55 -0700

    standardized tsconfig, added headlessui

--- packages/iframe/src/App.tsx
+++ packages/iframe/src/App.tsx
@@ -12,14 +12,17 @@ function App() {
         <div className="section single-row">
           <h1 className="font-bold">Sending messages</h1>
         </div>
-        <p className="section py-4">
+        <p className="section py-2">
           Open up the console in
           <br />
           <strong>Plugins &gt; Development &gt; Open console</strong>
           <br /> to see the sent message.
         </p>
-        <div className="section single-row mb-2">
-          <Button onClick={() => sendMessage(createTimeMessage())}>
+        <div className="section single-row my-2">
+          <Button
+            className="flex-1"
+            onClick={() => sendMessage(createTimeMessage())}
+          >
             Send message
           </Button>
         </div>
--- packages/sandbox/src/main.ts
+++ packages/sandbox/src/main.ts
@@ -5,7 +5,7 @@ const { x: x0, y: y0, height: h0 } = figma.viewport.bounds;
 
 const uiOptions: ShowUIOptions = {
   themeColors: true,
-  width: 320,
+  width: 240,
   height: Math.round(h0 - 96),
   position: { x: Math.round(x0 + 16), y: Math.round(y0 + 16) },
 };
