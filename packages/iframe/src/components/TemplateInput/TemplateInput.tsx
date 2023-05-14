90d4071860ab57ff74d0fc7dc235b14b50000adf
Philip Ting <pting.me+gh@gmail.com>
2023-05-13 19:05:48 -0700

    fixed listbox open

--- packages/iframe/src/components/TemplateInput/TemplateInput.tsx
+++ packages/iframe/src/components/TemplateInput/TemplateInput.tsx
@@ -23,30 +23,42 @@ export function TemplateInput() {
       value={selectedTemplate}
       onChange={setSelectedTemplate}
     >
-      <div className="relative whitespace-nowrap">
-        <Listbox.Button className="hover:bg-fill focus:bg-fill hover:border-stroke focus:border-stroke fill-icon-tertiary hover:fill-icon flex h-7 items-center justify-between rounded-sm bg-transparent px-1.5">
-          <div className="pr-1">{selectedTemplate.type}</div>
-          <div>
-            <ChevronIcon />
-          </div>
-        </Listbox.Button>
-        <Listbox.Options className="bg-fill-secondary absolute right-0 z-10 rounded py-2 outline-none">
-          {templates.map((template) => (
-            <Listbox.Option
-              key={JSON.stringify(template)}
-              value={template}
-              className={({ active }) =>
-                clsx(
-                  active && "bg-fill-brand-hover",
-                  "overflow-hidden text-ellipsis whitespace-nowrap px-2 py-1"
-                )
-              }
+      {({ open }) => {
+        return (
+          <div className="relative whitespace-nowrap">
+            <Listbox.Button
+              onClick={(e) => {
+                console.log(e);
+              }}
+              className={clsx(
+                open && "bg-fill border-stroke fill-icon",
+                "hover:bg-fill hover:border-stroke fill-icon-tertiary hover:fill-icon flex h-7 items-center justify-between rounded-sm border border-transparent bg-transparent px-1.5"
+              )}
             >
-              {template.type}
-            </Listbox.Option>
-          ))}
-        </Listbox.Options>
-      </div>
+              <div className="pr-1">{selectedTemplate.type}</div>
+              <div>
+                <ChevronIcon />
+              </div>
+            </Listbox.Button>
+            <Listbox.Options className="bg-fill-secondary border-stroke absolute right-0 z-10 rounded border py-2 outline-none">
+              {templates.map((template) => (
+                <Listbox.Option
+                  key={JSON.stringify(template)}
+                  value={template}
+                  className={({ active }) =>
+                    clsx(
+                      active && "bg-fill-brand-hover",
+                      "overflow-hidden text-ellipsis whitespace-nowrap px-2 py-1"
+                    )
+                  }
+                >
+                  {template.type}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </div>
+        );
+      }}
     </Listbox>
   );
 }
